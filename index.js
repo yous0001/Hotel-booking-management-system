6f0bad11ac0397103432eaa660ca94d838fa404b
Yousef emad mikhail <mad33833@gmail.com>
2025-06-29 00:13:40 +0300

    add welcome api and not found api

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 import express from "express";
 import dotenv from "dotenv";
 import chalk from "chalk";
+import cors from "cors";
 import { db_connection } from "./src/config/db_connection.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import { globalResponse } from "./src/middlewares/general-response.middleware.js";
 
 dotenv.config();
 
-
-const app=express()
+const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json())//enable us to get json data from the body
 
-app.use("/auth", authRoutes)
-
-
+app.use(cors());
+app.use(express.json());
 
 
-app.use(globalResponse)
-db_connection()
-app.listen(port, () => {
-        console.log(chalk.bgGreen(`Server is running on port ${port}`));
+app.get("/", (req, res) => {
+    res.send("Welcome to our hotel API.");
+});
+
+// API routes
+app.use("/auth", authRoutes);
+
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: "❌ Route not found",
+        statusCode: 404,
     });
+});
+
+app.use(globalResponse);
+
+
+db_connection();
+
+
+app.listen(port, () => {
+    console.log(chalk.bgGreen(`Server is running on port ${port}`));
+});
